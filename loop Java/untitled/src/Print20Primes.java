45ccbafd25ace49949e0500943fda368cc421195
HoangHai0156 <hoanghai41099@gmail.com>
2023-04-10 08:09:47 +0700

    10/04/23

---
+++ loop Java/untitled/src/Print20Primes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Print20Primes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        for (int i = 2; i < 1000; i++) {
+            int flag = 0;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    flag++;
+                }
+            }
+            if (count <= 20) {
+                if (flag == 1) {
+                    count++;
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
