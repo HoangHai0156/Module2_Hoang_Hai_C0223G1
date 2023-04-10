cfe5f4ec3dd6e2cb087f4dfc947233c28d2c40df
HoangHai0156 <hoanghai41099@gmail.com>
2023-04-10 14:01:30 +0700

    10/04/23

---
+++ array-and-method/src/DeleteValueInArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DeleteValueInArray {
+    public static void main(String[] args) {
+        int[] Numbers = {1,5,8,9,6,1,10,4,7};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mang hien tai la "+ Arrays.toString(Numbers));
+        System.out.println("Nhap vao so muon xoa khoi mang");
+        int number = scanner.nextInt();
+        boolean flag = false;
+        for (int i =0; i < Numbers.length ; i++){
+            if(number == Numbers[i]){
+                for(int j = i; j < Numbers.length-1; j++){
+                    Numbers[j]=Numbers[j+1];
+                }
+                Numbers[Numbers.length-1] = 0;
+                flag = true;
+            }
+        }
+        if(flag = false){
+            System.out.println("So vua nhap khong co trong mang");
+        }
+        System.out.println("Mang sau khi xoa la "+ Arrays.toString(Numbers));
+    }
+}
