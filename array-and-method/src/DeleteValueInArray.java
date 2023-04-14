0dafb3f55f5f6397ee4b6d3ecc5bffa7e7b54ebe
HoangHai0156 <hoanghai41099@gmail.com>
2023-04-14 11:04:19 +0700

    14/04/23

--- array-and-method/src/DeleteValueInArray.java
+++ array-and-method/src/DeleteValueInArray.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class DeleteValueInArray {
     public static void main(String[] args) {
-        int[] Numbers = {1,5,8,9,6,1,10,4,7};
+        int[] Numbers = {1,5,8,9,9,6,1,10,4,7,9};
         Scanner scanner = new Scanner(System.in);
         System.out.println("Mang hien tai la "+ Arrays.toString(Numbers));
         System.out.println("Nhap vao so muon xoa khoi mang");
         int number = scanner.nextInt();
         boolean flag = false;
         for (int i =0; i < Numbers.length ; i++){
-            if(number == Numbers[i]){
+            while (number == Numbers[i]){
                 for(int j = i; j < Numbers.length-1; j++){
                     Numbers[j]=Numbers[j+1];
                 }
