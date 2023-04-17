2a8c3af7c81362d78d88cdbc2878b78c59b125f9
HoangHai0156 <hoanghai41099@gmail.com>
2023-04-17 08:38:01 +0700

    17/04/23

---
+++ Access Modifers/src/Practice2/Student.java
@@ -0,0 +1,17 @@
+package Practice2;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+    public Student(){};
+
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+}
---
+++ Access Modifers/src/Practice2/Test.java
@@ -0,0 +1,9 @@
+package Practice2;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Hai");
+        student.setClasses("C0223G1");
+    }
+}
