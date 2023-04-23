0bd337653cd97f22d20c6559825b2da2b86fc96b
HoangHai0156 <hoanghai41099@gmail.com>
2023-04-23 20:01:22 +0700

    23/04/23

---
+++ List/src/thuc_hanh_1/MyListTest.java
@@ -0,0 +1,22 @@
+package thuc_hanh_1;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println(listInteger.getElement(4));
+        System.out.println(listInteger.getElement(1));
+        System.out.println(listInteger.getElement(2));
+
+//        System.out.println(listInteger.getElement(6));
+
+        System.out.println(listInteger.getElement(-1));
+    }
+}
