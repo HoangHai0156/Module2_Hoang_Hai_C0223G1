8abdfc925e3b6372001c7d4029134b38717a107a
HoangHai0156 <hoanghai41099@gmail.com>
2023-04-25 14:56:40 +0700

    25/04/23

---
+++ Stack and Queue/src/build_queue_use_circularLinkedList/CircularLikedListClient.java
@@ -0,0 +1,18 @@
+package build_queue_use_circularLinkedList;
+
+
+public class CircularLikedListClient {
+    public static void main(String[] args) {
+        MyCircularLinkedListQueue myLinkedListQueue = new MyCircularLinkedListQueue();
+        myLinkedListQueue.enQueue(10);
+        myLinkedListQueue.enQueue(20);
+
+        System.out.println(myLinkedListQueue.deQueue().getData());
+        System.out.println(myLinkedListQueue.deQueue().getData());
+        System.out.println(myLinkedListQueue.deQueue().getData());
+
+        myLinkedListQueue.enQueue(30);
+        myLinkedListQueue.enQueue(40);
+        myLinkedListQueue.enQueue(50);
+    }
+}
