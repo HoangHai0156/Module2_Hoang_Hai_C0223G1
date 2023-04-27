6dcee229c46d8f809e351570aee2f0aa810852f7
HoangHai0156 <hoanghai41099@gmail.com>
2023-04-27 14:48:59 +0700

    27/04/23

---
+++ Map and Tree/src/thuc_hanh_3/Student.java
@@ -0,0 +1,51 @@
+package thuc_hanh_3;
+
+public class Student implements Comparable<Student>{
+    private String name;
+    private int age;
+    private String homeTown;
+
+    public Student(String name, int age, String homeTown) {
+        this.name = name;
+        this.age = age;
+        this.homeTown = homeTown;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHomeTown() {
+        return homeTown;
+    }
+
+    public void setHomeTown(String homeTown) {
+        this.homeTown = homeTown;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", homeTown='" + homeTown + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.getName().compareToIgnoreCase(o.getName());
+    }
+}
